0e3206587ba1827dc6b06c61b5d334b69fcf49ca
A19B75345 educacion <A19B75345@educacion.edu.do>
2021-05-03 10:15:57 -0400

    Limpia y agrega informacion a README.md

---
+++ src/config/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'Firebase/ app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Incializar firebase
+
+firebase.initializeApp({
+ /*
+    Esta informacion te la entregar en tu proyecto firebase y es distinta en cada proyecto.
+
+        M
+
+*/
+});
